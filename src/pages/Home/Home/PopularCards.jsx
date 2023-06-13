71ec29f2ab95406f7b5371fe722475027f8b6e60
shanto <santawkha@gmail.com>
2023-06-13 18:42:45 +0600

    claass added

--- src/pages/Home/Home/PopularCards.jsx
+++ src/pages/Home/Home/PopularCards.jsx
@@ -1,16 +1,16 @@
 
 
 const PopularCards = ({ course }) => {
-    const { Image, name, Instructor_Name, Price, Available_seats } = course
+    const { image, ClassName, instructorName, price, seats } = course
     return (
 
         <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={Image} alt="Shoes" /></figure>
+            <figure><img src={image} alt="Shoes" /></figure>
             <div className="card-body">
-                <h2 className="card-title">{name}</h2>
-                <p>Instructor Name:{Instructor_Name}</p>
-                <p>Price:{Price}$</p>
-                <p>Available Seats:{Available_seats}</p>
+                <h2 className="card-title">{ClassName}</h2>
+                <p>Instructor Name:{instructorName}</p>
+                <p>Price:{price}$</p>
+                <p>Available Seats:{seats}</p>
                 <div className="card-actions justify-end">
                     <button className="btn btn-primary">Add to Favourite</button>
                 </div>
