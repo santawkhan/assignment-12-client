2448e68087d2d0a01c512ec7ae64c140ffa21bc7
shanto <santawkha@gmail.com>
2023-06-14 23:49:45 +0600

    usedata added

--- src/hooks/useData.jsx
+++ src/hooks/useData.jsx
@@ -4,7 +4,15 @@ import { AuthContext } from "../providers/AuthProvider";
 
 
 const useData = () => {
-
+    const { user } = useContext(AuthContext);
+    const { data: Items = [], isLoading: loading, refetch } = useQuery({
+        queryKey: ['Items'],
+        queryFn: async () => {
+            const res = await fetch(`https://summer-camp-server-santawkhan.vercel.app/Course?email=${user?.email}`);
+            return res.json();
+        }
+    })
+    return [Items, loading, refetch]
 };
 
 export default useData;
